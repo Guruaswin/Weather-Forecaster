73cf49aabd8bb7f8d75008f32c86ea6f13af8551
Guru <guruaswin61@gmail.com>
2022-01-16 20:49:51 +0530

    First page completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import earth from './earth.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>Hi there</h1>
+      <div className='firstPage'>
+        <div className='navBar'></div>
+        <div className='leftTab'>
+          <img src={earth} className='leftTab_image'/>
+          <div className='leftTab_bottom'>
+            <div className='search'>
+              <button className='searchButton'>Search</button>
+              <input className='inputBar' type='text' placeholder='Enter location'/>
+            </div>
+            <div className='city'></div>
+          </div>
+        </div>
+        <div className='rightTab'>
+          <div className='rightTop'></div>
+          <div className='rightBottom'></div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
