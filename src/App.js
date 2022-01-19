0cc12da6bb09d757870c50b203135e85637e53e1
Guru <guruaswin61@gmail.com>
2022-01-19 11:30:31 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import earth from './earth.jpg';
+// import earth from './earth.jpg';
+import earth2 from './earth2.jpg';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function App() {
   return (
     <div className="App">
-      <div className='firstPage'>
+      {/* <div className='firstPage'>
         <div className='navBar'></div>
         <div className='leftTab'>
           <img src={earth} className='leftTab_image'/>
@@ -22,7 +24,31 @@ function App() {
           <div className='rightBottom'></div>
         </div>
       </div> 
-      {/* <div className='FeedBack_Page'></div> */}
+      <div className='FeedBack_Page'></div> */}
+      <div className="firstPage">
+        <div className="topSection">
+          <div className="menuBar">
+            <GiHamburgerMenu className='hamBurger'/>
+          </div>
+          <div className="navBar">
+            <ul className='login'>Guest</ul>
+          </div>
+        </div>
+        <div className="bodySection">
+            <div className="leftTab">
+              <div className="earth">
+                <img src={earth2} alt="Earth" className='earthImage'/>
+              </div>
+              <div className="searchCity">
+                <button className='searchButton'>Search</button>
+                <input type="text" className='inputBar' placeholder='Enter city' />
+              </div>
+            </div>
+            <div className="rightTab">
+              
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
