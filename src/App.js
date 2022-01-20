59fc067521210c52cfc7e9d432caae9366d44000
Dharun P <pdharun25@gmail.com>
2022-01-20 18:55:50 +0530

    Login box
    added "continue with email " tab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import earth from './earth.jpg';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function App() {
   return (
     <div className="App">
-      <div className='firstPage'>
+      {/* <div className='firstPage'>
         <div className='navBar'></div>
         <div className='leftTab'>
           <img src={earth} className='leftTab_image'/>
@@ -21,7 +22,19 @@ function App() {
           <div className='rightTop'></div>
           <div className='rightBottom'></div>
         </div>
-      </div> 
+      </div>  */}
+      {/*
+      For login page from line 29
+      */}
+      <div className="menuBar">
+            {/*Hamburger Icon*/}
+            <GiHamburgerMenu className='hamBurger'/>
+      </div>
+      {/*Main Box in the middle*/}
+      <div className="CentreBox">
+        {/*Continue with google tab*/}
+        <input className="Continue" placeholder='Continue with Google?' type={Text}/>
+      </div>
     </div>
   );
 }
