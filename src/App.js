8c4d52d479c2e0c212d89a959e1136b254cf87b3
Dharun P <pdharun25@gmail.com>
2022-01-28 19:59:41 +0530

    Added from guru
    Added folders and major change from guru branch

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import earth from './earth.jpg';
-import { GiHamburgerMenu} from 'react-icons/gi';
-import {GrClose} from 'react-icons/gr';
+import FirstPage from './components/js/FirstPage';
+import LoginPage from './components/js/LoginPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      {/* <div className='firstPage'>
-        <div className='navBar'></div>
-        <div className='leftTab'>
-          <img src={earth} className='leftTab_image'/>
-          <div className='leftTab_bottom'>
-            <div className='search'>
-              <button className='searchButton'>Search</button>
-              <input className='inputBar' type='text' placeholder='Enter location'/>
-            </div>
-            <div className='city'></div>
-          </div>
-        </div>
-        <div className='rightTab'>
-          <div className='rightTop'></div>
-          <div className='rightBottom'></div>
-        </div>
-      </div>  */}
-      {/*
-      For create account page from line 29
-      */}
-      <div className="menuBar">
-            {/*Hamburger Icon*/}
-            {/* <GiHamburgerMenu className='hamBurger'/> */}
-            <div className='closeIcon'><GrClose className='GrClose'/></div>
-            {/*side menubar*/}
-            <div className='home'>
-               <h3>Home</h3>
-            </div>
-            <div className='settings'>
-               <h3>Settings</h3>
-            </div>
-            <div className='profile'>
-               <h3>Profile</h3>
-            </div>
-            <div className='favorites'>
-               <h3>Favorites</h3>
-            </div>
-      </div>
-      {/*Create Account left Box*/}
-      {/* <div className="LeftTab">
-        
-      </div> */}
-    </div>
+    <>
+      {/* <FirstPage /> */}
+      <LoginPage />
+    </>
   );
 }
 
